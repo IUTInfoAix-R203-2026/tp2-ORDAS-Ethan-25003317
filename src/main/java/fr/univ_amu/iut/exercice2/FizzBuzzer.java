e5c47d3b0cdf4f2922fb53509ad2065272439e2f
Ethan-ORDAS <ethan.ordas@etu.univ-amu.fr>
2026-05-07 09:32:05 +0200

    Exercice2 (#2)
     exercice 2 fini tout les test sont validés

--- src/main/java/fr/univ_amu/iut/exercice2/FizzBuzzer.java
+++ src/main/java/fr/univ_amu/iut/exercice2/FizzBuzzer.java
@@ -28,6 +28,9 @@ public class FizzBuzzer {
     // return par défaut. Activez les tests dans l'ordre : 1 et 2 passent
     // directement (fake it via le return final), 3 demande d'introduire
     // un premier cas, etc.
+    if (n % 3 == 0 && n % 5 == 0) return "FizzBuzz";
+    if (n % 3 == 0) return "Fizz";
+    if (n % 5 == 0) return "Buzz";
     return String.valueOf(n);
   }
 
@@ -41,6 +44,21 @@ public class FizzBuzzer {
     String[] sequence = new String[n];
     // TODO exercice 2 : remplir sequence[i] en réutilisant fizzBuzz(i+1).
     // Ne dupliquez pas la logique : appelez fizzBuzz !
+    for (int i = 1; i < n + 1; i += 1) {
+      if (i % 3 == 0 && i % 5 == 0) {
+        sequence[i - 1] = "FizzBuzz";
+        continue;
+      }
+      if (i % 3 == 0) {
+        sequence[i - 1] = "Fizz";
+        continue;
+      }
+      if (i % 5 == 0) {
+        sequence[i - 1] = "Buzz";
+        continue;
+      }
+      sequence[i - 1] = String.valueOf(i);
+    }
     return sequence;
   }
 }
